edbfca6535ce285aaef42cb9120fa0ffcb1cf214
Paulo Giovanny <paulo.giovanny@presidencia.gov.br>
2020-04-08 10:42:51 -0300

    Alterado endereço da api apontando para docker

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://10.24.10.233:8000/api',
+  baseURL: 'http://10.24.10.233:8080/api',
   //baseURL: 'http://192.168.1.186:3333',
 })
 export default api;
